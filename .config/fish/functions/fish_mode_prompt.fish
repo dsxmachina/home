78b06ffc401466d62dc77d63558e26f7eefd0a7b
Christian Hammermeister <christian.hammermeister@tu-dortmund.de>
2020-01-21 14:51:05 +0100

    new fish features

--- .config/fish/functions/fish_mode_prompt.fish
+++ .config/fish/functions/fish_mode_prompt.fish
@@ -1,4 +1,4 @@
-function fish_mode_prompt --description 'Displays the current mode'
+function fish_mode_prompt
    # Do nothing if not in vi mode
     if test "$fish_key_bindings" = "fish_vi_key_bindings"
         switch $fish_bind_mode
@@ -6,16 +6,15 @@ function fish_mode_prompt --description 'Displays the current mode'
                 set_color --bold red
                 echo [n]
             case insert
-                set_color --bold green
-                echo [i]
+                set_color normal
+                #echo [i]
             case replace_one
                 set_color --bold green
                 echo [r]
             case visual
-                set_color --bold blue
+                set_color blue
                 echo [v]
         end
         set_color normal
-        printf " "
     end
 end
--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,46 +1,22 @@
-function fish_prompt --description 'Write out the prompt, prepending the Debian chroot environment if present'
-	if not set -q __fish_prompt_normal
-        set -g __fish_prompt_normal (set_color normal)
+function fish_prompt
+    # https://stackoverflow.com/questions/24581793/ps1-prompt-in-fish-friendly-interactive-shell-show-git-branch
+#    set -l git_branch (git branch 2>/dev/null | sed -n '/\* /s///p')
+    set -l git_status (git status --porcelain | grep 'M ')
+    set_color normal
+    echo -n (whoami)'@'(hostname)':'
+    set_color $fish_color_cwd
+    echo -n (prompt_pwd)
+    if [ -n "$git_status" ] 
+        set_color --bold red
+    	#printf "\u25a0"
+	#printf " \U25CF"
+	printf "+"
+    else
+        set_color --bold green
+	#printf " \U25CF"
+	printf "-"
     end
-
-    if not set -q __fish_prompt_chroot_env
-        set -g __fish_prompt_chroot_env (set_color yellow)
-    end
-
-    # Set variable identifying the chroot you work in (used in the prompt below)
-    if not set -q debian_chroot
-        and test -r /etc/debian_chroot
-        set debian_chroot (cat /etc/debian_chroot)
-    end
-    if not set -q __fish_debian_chroot_prompt
-        and set -q debian_chroot
-        and test -n "$debian_chroot"
-        set -g __fish_debian_chroot_prompt "($debian_chroot)"
-    end
-
-    # Prepend the chroot environment if present
-    if set -q __fish_debian_chroot_prompt
-        echo -n -s "$__fish_prompt_chroot_env" "$__fish_debian_chroot_prompt" "$__fish_prompt_normal" ' '
-    end
-
-    switch "$USER"
-        case root toor
-            if not set -q __fish_prompt_cwd
-                if set -q fish_color_cwd_root
-                    set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
-                else
-                    set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-                end
-            end
-
-            echo -n -s "$USER" @ (prompt_hostname) ' ' "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" '# '
-
-        case '*'
-            if not set -q __fish_prompt_cwd
-                set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-            end
-
-            echo -n -s "$USER" @ (prompt_hostname) ' ' "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" '> '
-
-    end
+    set_color normal
+    echo -n '> '
 end
+
